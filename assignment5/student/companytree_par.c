aa37ee6672f72b9ff4231497a8ace9a5d050cf22
Sebastian Rossner <git@rossner.in>
2017-06-16 18:00:44 +0200

    5 & 6 done

--- assignment5/student/companytree_par.c
+++ assignment5/student/companytree_par.c
@@ -1,28 +1,55 @@
 #include "companytree.h"
+#include <math.h>
+
+
+int is_prime_my(int n)
+{
+  int i, flag = 1;
+
+  for(i = 2; i <= sqrt(n); ++i)
+  {
+    if (n % i == 0)
+    {
+      flag = 0;
+      break;
+    }
+  }
+
+  return flag;
+}
+
+
+int compute_workHours_my(int data)
+{
+  // nothing meaningfull, just a function that is kind of computationally expensive
+  // such that parallelising makes sense :)
+
+    int i; int sum = 11;
+      for (i = 28; i < data; i++){
+      if (is_prime_my(i)) sum++;
+    }
+
+  return (70 + (sum % 100)*(sum % 100)*(sum % 100)*(sum % 100)/1000000)*10;
+}
 
 
 void traverse(tree *node, int numThreads){
 
-{
 
-        omp_set_max_active_levels( numThreads );
     if(node != NULL){
+    #pragma omp parallel sections shared(node, numThreads)
+        {
 
-        tree *right = node->right;
-        tree *left = node->left;
+            #pragma omp section
+            traverse(node->right, numThreads);
 
+            #pragma omp section
+            traverse(node->left, numThreads);
 
-        #pragma omp task shared(right, numThreads)
-        traverse(right, numThreads);
+        }
 
-        #pragma omp task shared(left, numThreads)
-        traverse(left, numThreads);
+        top_work_hours[node->id] = compute_workHours_my(node->data);
 
-        node->work_hours = compute_workHours(node->data);
-        top_work_hours[node->id] = node->work_hours;
     }
-}
-
-
 }
 
--- assignment6/student/companytree_par.c
+++ assignment6/student/companytree_par.c
@@ -1,32 +1,50 @@
 #include "companytree.h"
-#include <omp.h>
-#include "stdio.h"
+#include <math.h>
+
+int is_prime_my(int n)
+{
+  int i, flag = 1;
+
+  for(i = 2; i <= sqrt(n); ++i)
+  {
+    if (n % i == 0)
+    {
+      flag = 0;
+      break;
+    }
+  }
+
+  return flag;
+}
+
+int compute_workHours_my(int data)
+{
+  // nothing meaningfull, just a function that is kind of computationally expensive
+  // such that parallelising makes sense :)
+
+  int i; int sum = 11;
+    for (i = 28; i < data; i++){
+      if (is_prime_my(i)) sum++;
+    }
+
+  return (70 + (sum % 100)*(sum % 100)*(sum % 100)*(sum % 100)/1000000)*10;
+}
 
 void traverse(tree *node, int numThreads){
 
-
     if(node != NULL){
-        #pragma omp parallel sections shared(node, numThreads)
-        {
 
-#pragma omp section
-            traverse(node->right, numThreads);
+        tree *right = node->right;
+        tree *left = node->left;
 
-#pragma omp section
-            traverse(node->left, numThreads);
+        #pragma omp task shared(right, numThreads)
+        traverse(right, numThreads);
 
+        #pragma omp task shared(left, numThreads)
+        traverse(left, numThreads);
 
-
-#pragma omp section
-        node->work_hours = compute_workHours(node->data);
-        }
-        top_work_hours[node->id] = node->work_hours;
-
-
-
+        top_work_hours[node->id] = compute_workHours_my(node->data);
 
     }
-
-
 }
 
