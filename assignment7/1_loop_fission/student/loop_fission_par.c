04b503e51332c2b8559a5b730647b1d06521b506
Sebastian Rossner <git@rossner.in>
2017-07-14 10:02:58 +0200

    solved 7 1

--- assignment7/1_loop_fission/student/loop_fission_par.c
+++ assignment7/1_loop_fission/student/loop_fission_par.c
@@ -1,13 +1,21 @@
 
 void compute(unsigned long **a, unsigned long **b, unsigned long **c, unsigned long **d, int N, int num_threads) {
 
-	// perform loop fission to transform this loop and parallelize it with OpenMP
-	for (int j = 1; j < N; j++) {
-		for (int i = 1; i < N; i++) {
-			a[i][j]   = a[i][j] * b[i][j];
-			b[i][j + 1] = 2 * a[i][j] * c[i - 1][j];
-			c[i][j] = 3 * d[i][j];
-			d[i][j]   = 2 * c[i + 1][j];
-		}
-	}
+    #pragma omp parallel num_threads(num_threads)
+
+    for (int i = 1; i < N; i++) {
+        #pragma omp for
+        for (int j = 1; j < N; j++) {
+            c[i][j] = 3 * d[i][j];
+            d[i][j]   = 2 * c[i + 1][j];
+        }
+    }
+
+    #pragma omp for
+    for (int i = 1; i < N; i++) {
+        for (int j = 1; j < N; j++) {
+            a[i][j]   = a[i][j] * b[i][j];
+            b[i][j + 1] = 2 * a[i][j] * c[i - 1][j];
+        }
+    }
 }
